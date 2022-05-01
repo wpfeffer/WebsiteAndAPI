b282537d34c8f572b6d8ebd8e16e81b0419c95f5
wpfeffer <waynee.pfeffer@gmail.com>
2022-05-01 07:01:16 -0500

    Added and updated code.

--- WebsiteAndAPI/main.go
+++ WebsiteAndAPI/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	courses := make([]string, 5, 10)
+
+	fmt.Printf("Length of slice is %d and capacity is %d\n", len(courses), cap(courses))
+
+	strings := make(map[string]string)
 }
